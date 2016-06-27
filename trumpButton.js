6d498b8c03019a503f626c2e791f1820aa92ee6f
timchu <timchu>
2016-06-27 01:05:15 -0400

    More issues and updated some.

--- trumpButton.js
+++ trumpButton.js
@@ -18,20 +18,26 @@ function arrToObject(array) {
 
 
 // Have all of Trump's positions in 2 length arrays.
-var trumpImmigrationArr = ["I love immigration.", "Public reports routinely state great amounts of crime are being committed by illegal immigrants. This must be stopped and it must be stopped now."]
-
+var trumpImmigrationArr = ["I love immigration. Immigrants come to this country, they want to work hard, be successful, raise their kids, and share in the American dream. It's a beautiful story. I can close my eyes and just imagine what my relatives must have been thinking when they sailed past the Statue of Liberty into New York and their new lives. And if they could only see the results of their risk and sacrifice! How can anyone not appreciate the courage it took for these people to leave their families and come here?", "Build the Wall."]
 var trumpImmigration = arrToObject(trumpImmigrationArr);
 
-var trumpNukesArr = ["It's a horror to use nuclear weapons. The power of weaponry today is the single greatest problem that our world has", "I will be the last to use (nuclear weapons)....But I will never, ever rule it out."]
+var trumpNukesArr = ["I will be the last to use (nuclear weapons)....But I will never, ever rule it out.", "I've always thought about the issue of nuclear war; it's a very important element in my thought process. It's the ultimate, the ultimate catastrophe, the biggest problem this world has, and nobody's focusing on the nuts and bolts of it. It's a little like sickness. People don't believe they're going to get sick until they do. Nobody wants to talk about it. I believe the greatest of all stupidities is people's believing it will never happen, because everybody knows how destructive it will be, so nobody uses weapons. What bullshit."]
 var trumpNukes = arrToObject(trumpNukesArr);
 
 trumpHillaryArr = ["Hillary Clinton I think is a terrific woman. I am biased because I have known her for years. I live in New York. She lives in New York. I really like her and her husband both a lot. I think she really works hard. And I think, again, she's given an agenda, it is not all of her, but I think she really works hard and I think she does a good job. I like her.", "The Hillary Clinton foreign policy has cost America thousands of lives and trillions of dollars -- and unleashed ISIS across the world. No secretary of state has been more wrong, more often, and in more places than Hillary Clinton. Her decisions spread death, destruction and terrorism everywhere she touched. "]
-
 var trumpHillary = arrToObject(trumpHillaryArr);
 
 var trumpHispanicsArr = ["Happy #CincoDeMayo! The best taco bowls are made in Trump Tower Grill. I love Hispanics!", "When Mexico sends its people, they're not sending their best.... They're rapists. And some, I assume, are good people."]
-
 var trumpHispanics = arrToObject(trumpHispanicsArr);
+
+var trumpHealthcareArr = ["Well, I like the mandate.", "Repeal all of #Obamacare, including the mandate, period."]
+var trumpHealthcare = arrToObject(trumpHealthcareArr);
+
+var trumpRefugeesArr = ["You know, it's living in hell in Syria. There's no question about it. They're living in hell, and something has to be done. I hate the concept of it, but on a humanitarian basis, you have to (take in refugees).", "Donald J. Trump is calling for a total and complete shutdown of Muslims entering the United States until our country's representatives can figure out what is going on."]
+var trumpRefugees = arrToObject(trumpRefugeesArr);
+
+var trumpLGBTArr = ["North Carolina did something that was very strong (in banning transgender people from using their bathroom of choice). And they're paying a price. They should leave it the way it is.", "I believe it should be states' rights and the state should make the decision. They're more capable of making the decision."]
+var trumpLGBT = arrToObject(trumpLGBTArr);
 
 function makeTrumpArg(issue) {
   issueTitle = capitalize(issue);
@@ -48,10 +54,10 @@ var trump = {
   nukes: trumpNukes,
   hillary: trumpHillary,
   hispanics: trumpHispanics,
-  refugees: makeTrumpArg('refugees'),
-  healthcare: makeTrumpArg('healthcare'),
+  refugees: trumpRefugees,
+  healthcare: trumpHealthcare,
   trade: makeTrumpArg('trade'),
-  lgbt_rights: makeTrumpArg('LGBT rights'),
+  lgbt_rights: trumpLGBT,
   economy: makeTrumpArg('economy'),
 };
 
