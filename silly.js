7ce3dcdbce8eddd7d865a10a5396e4cde42265e7
timchu <timchu>
2016-06-27 23:26:01 -0400

    Deleted test files.

--- silly.js
+++
@@ -1,6 +0,0 @@
-$(document).ready( function () {
-  $("#butt").css('cursor','pointer');
-  $(document).on('click touchstart', "#butt",  function(event) {
-      alert("This click worked on iOS!");
-});
-} 
